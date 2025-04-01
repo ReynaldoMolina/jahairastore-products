c85c3595dd9530bf1fc84f148734b1de0e9af568
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-04-01 10:18:32 -0600

    Added footer, info section, product section

---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+export const socialmedia = [
+  {
+    name: 'facebook',
+    url: 'https://www.facebook.com/profile.php?id=61560253337819'
+  },
+  {
+    name: 'tiktok',
+    url: 'https://www.tiktok.com/@jahaira_store'
+  },
+  {
+    name: 'whatsapp',
+    url: 'https://api.whatsapp.com/send?phone=%2B50589531428'
+  }
+];
+
+export const faq = [
+  {
+    question: '¿Cómo uso la página?',
+    answer: 'Agrega productos al carrito, luego compártenos el link del carrito por WhatsApp.'
+  },
+  {
+    question: '¿Cuánto tarda el envío?',
+    answer: 'Hay dos tipos de envío, por barco y aéreo. Por barco tarda 1 mes, por avión 1 semana.'
+  },
+  {
+    question: '¿Qué métodos de pago ofrecen?',
+    answer: 'Aceptamos efectivo, depósitos a Banpro y Lafise.'
+  },
+  {
+    question: '¿Debo dar algo para poder hacer la compra?',
+    answer: 'Para asegurar la compra de su pedido pedimos un abono del 50%.'
+  },
+]
